b3e7321084a6123ca49b6554b31723d821973fef
Thomas Bakken <tbakken14@gmail.com>
2023-10-10 01:50:09 -0700

    Update main to convert inputs and print output

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 from InputControl import *
+from TemperatureConverter import *
 
+TEMPERATURE_CONVERTER_MAP = {"F" : {"K" : fahrenheitToKelvin, "C" : fahrenheitToCelsius},
+                 "C" : {"K" : celsiusToKelvin, "F" : celsiusToFahrenheit},
+                 "K" : {"F" : kelvinToFahrenheit, "C" : kelvinToCelsius}}
 def main():
-    fromUnit, toUnit, temperature = getValidInputs()
+    fromUnit, toUnit, fromTemperature = getValidInputs()
+    toTemperature = TEMPERATURE_CONVERTER_MAP[fromUnit][toUnit](float(fromTemperature))
+    print("%.2f %s = %.2f %s" % (fromTemperature, fromUnit, toTemperature, toUnit))
 
-main()
+if __name__ == "__main__":
+    main()
